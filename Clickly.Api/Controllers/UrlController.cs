e1588f9fe58b8d7ac8bbdcf58ba8f4c1609bdcc1
Umut Kaan Kartaloğlu <umutkaankartaloglu9@gmail.com>
2025-09-06 16:44:15 +0300

    (feat)Api:add controllers for urlcontroller.cs and (refactor)Api:program.cs

---
+++ Clickly.Api/Controllers/UrlController.cs
@@ -0,0 +1,32 @@
+﻿using Clickly.Application.Features.GetStats;
+using Clickly.Application.Features.ShortenUrl;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clickly.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UrlController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public UrlController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("shorten")]
+       public async Task<IActionResult> ShortenUrl([FromBody] ShortenUrlCommand command)
+        {
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
+
+        public async Task<IActionResult> GetStats(string shortCode)
+        {
+            var response = await _mediator.Send(new GetStatsQuery { ShortCode = shortCode });
+            return Ok(response);
+        }
+    }
+}
--- Clickly.Api/Program.cs
+++ Clickly.Api/Program.cs
@@ -1,6 +1,17 @@
+using Clickly.Infrastructure.ServiceRegistration;
+using MediatR;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//API katmanı özgü servisler eklenecek
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Dİğer katman servisleri ekleneck
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureService(builder.Configuration);
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -9,33 +20,26 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
+// URL kısaltma end-point'i için yönlendirme (URL redirection)
+app.MapGet("/{shortCode}", async (string shortCode, IMediator mediator) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    var originalUrl = await mediator.Send(new Clickly.Application.Features.GetUrl.GetUrlQuery
+    {
+        ShortCode = shortCode,
+        // HttpContext'ten IP, User-Agent gibi bilgileri alıp buraya ekleyeceğiz
+    });
 
+    return Results.Redirect(originalUrl, permanent: false);
+});
+
+app.MapControllers();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
