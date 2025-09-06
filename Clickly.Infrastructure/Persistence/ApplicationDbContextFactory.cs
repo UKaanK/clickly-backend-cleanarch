1ee9948553a3dc88a6c549c5abccd8d3e4582053
Umut Kaan Kartaloğlu <umutkaankartaloglu9@gmail.com>
2025-09-06 17:36:01 +0300

    (feat) Infrastructure:add Click.cs and Url.cs migration

---
+++ Clickly.Infrastructure/Persistence/ApplicationDbContextFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clickly.Infrastructure.Persistence
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            // Startup projesinin appsettings.json dosyasını oku
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "..\\Clickly.Api")) // Clickly.Api projesinin yolu
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+    }
+
